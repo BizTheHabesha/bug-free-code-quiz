33f0981cc77acb4ac58f10efcc84c9ea2fd91ae3
Biz <github@bisrat.dev>
2023-03-14 19:47:59 -0600

    add css/js vars

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+var questionCardEls = document.getElementsByClassName('question-card');
+var introCardEl = document.getElementById('intro-card');
+var trackerCardEl = document.getElementById('intro-card');
+var numQuestions = questionCardEls.length;
