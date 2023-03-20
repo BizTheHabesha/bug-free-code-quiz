08b1f40878f8f5de5b797b4662951a15fb766aed
Biz <github@bisrat.dev>
2023-03-19 19:20:29 -0600

    increase comment verbosity

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,27 +21,38 @@ var timerInterval;
 // create a globally scoped variable to check if we are in review mode or not. This disables some
 // features that should only be active during a game.
 var reviewMode = false;
+// an array to store highscores within the code, outside of just localStorage. This way we can easily sort
+// the highscores.
 var highscoresArr = [];
+// a global variable to ensure the highscore isn't set twice in one session.
 var highscoreSet = false;
 // user gains 10 pts for a correctly answered question, looses 5 second for an incorrectly 
 // answered question. then, if correct, the current question is hidden and the next question displays
 // final score is the sum of seconds left and points gained
 function main(){
-    // initialize number of questions, subtracting one because the intro card is also a
+    // initialize number of questions, subtracting 1 because the intro card is also a
     // question-card for styling purposes
     for(let tqs of document.getElementsByClassName('total-questions')){
         tqs.textContent = questionCardEls.length - 1;
     }
+    // store all scores from local storage in the array defined above. hard-coded to stop at 10 entries
+    // because localStorage should not have more than 10 entries.
     for(let i = 0; i < 10; i++){
+        // stop after we have found the first empty score, otherwise we know we've stored all 10
         if(localStorage.getItem(`highscore-${i}`) === null){
             console.log(`loaded ${i} highscore(s)`);
             break;
         }else{
+            // push the scores into the array
             highscoresArr.push(JSON.parse(localStorage.getItem(`highscore-${i}`)));
         }
     }
+    // after all scores are stored, we sort the array using a custom function. this is nescceary because
+    // the default sort function is function(a,b){return b-a;}
     highscoresArr.sort(function(a,b){return b['scoreIn'] - a['scoreIn'];})
+    // debug tool to check that the array has indeed loaded all score from localStorage
     console.log(localStorage.length)
+    // at the start of the game, the timer hasn't started. let's take this opportunity to display instructions
     timerEl.textContent = 'Press the \'Start Quiz\' button to begin.'
     // initialize onclick for answer buttons
     for (let button of document.getElementsByClassName('question-answer')) {
@@ -105,12 +116,14 @@ function main(){
             }
         });
     }
-    document.getElementById('try-again').addEventListener("click", function(event){
+    // the 'try again' button just reloads the page, effectivly ending the current session
+    document.getElementById('try-again').addEventListener("click", function(){
         window.location.reload();
     })
+    // set event listener for the start button, which starts the timer among other things.
     document.getElementById('start').addEventListener('click', function(e){
         e.preventDefault();
-        // Initialize timer
+        // Initialize timer on screen
         timerEl.textContent = " " + secondsLeft + " seconds remaining.";
         // Sets interval in variable such that the interval counts in seconds
         timerInterval = setInterval(function() {
@@ -128,6 +141,7 @@ function main(){
             }
             // alternative text styling at specified time
             if(secondsLeft <= lastFewSeconds){
+                // use root psuedoclass to style this portion, as well as the rest.
                 timerLiEl.style.color = getComputedStyle(rootCSS).getPropertyValue('--danger-color-focus');
                 // font weight cannot be animated in vanilla CSS. Using text shadow mostly gives
                 // the intended effect, however.
@@ -144,35 +158,58 @@ function main(){
                 review('time');
             }
         }, 1000);
-    })  
+    })
+    // initialize event listener to sort scores, among other things.
     document.getElementById('save-score').addEventListener('click',function(e){
+        // prevent the page from reloading.
         e.preventDefault()
+        // ensure the user hasn't set a highscore already.
         if(!highscoreSet){
+            // a variable to indicate wether we were able to store it in one of the 10 empty slots
+            // in localStorage
             let scorePlaced = false;
+            // an object to store the intials of the player, their score, and the score's index in localStorage
             let newScore = {
                 initials: document.getElementById('initials').value,
                 scoreIn: score,
                 storageIndex: 0
             };  
+            // loop through the 10 possible slots of localStorage
             for(let i = 0; i < 10; i++){
+                // check if one of these slots is empty
                 if(localStorage.getItem(`highscore-${i}`) === null){
+                    // if we find an empty localStorage slot, add that index to the object
                     newScore.storageIndex = i;
+                    // insert the score into localStorage
                     localStorage.setItem(`highscore-${i}`,JSON.stringify(newScore));
+                    // insert the score into the array
                     highscoresArr.push(newScore);
+                    // sort the array using the same custom function above.
                     highscoresArr.sort(function(a,b){return b['scoreIn'] - a['scoreIn'];});
+                    // indicate the score was able to be placed
                     scorePlaced = true;
                     break;
                 }
             }
+            // in the event the score wasn't placed in one of the 10 slots, we remove the lowest score.
             if(!scorePlaced){
+                // we add it to the arrau
                 highscoresArr.push(newScore);
+                // sort the array
                 highscoresArr.sort(function(a,b){return b['scoreIn'] - a['scoreIn'];});
+                // then remove from localStorage and the array the item that was at the end of the array.
+                // because the array is already sorted, we know the last element will be the lowest score.
                 localStorage.removeItem(`highscore-${highscoresArr.pop().storageIndex}`);
             }
+            // indicate that the highscore has been set for this session.
             highscoreSet = true;
+        // if the user has already set a score, then display the error message and fade it out.
         }else{
+            // get the error message element
             hsem = document.getElementById('score-saved-error-message');
+            // display the element by giving it a color other than transparent
             hsem.style.color = getComputedStyle(rootCSS).getPropertyValue('--danger-color');
+            // fade out the message after 2 seconds.
             hsemInterval = setInterval(function(){
                 hsem.style.color = 'transparent';
                 clearInterval(hsemInterval);
@@ -231,39 +268,50 @@ function review(src){
     }
     // by default, once the game ends, toggle all cards on for review
     toggleCards(false);
+    // initialize event listener to clear highscores
     document.getElementById('clear-highscores').addEventListener('click', function(e){
         e.preventDefault();
+        // clear the localStorage
         localStorage.clear()
+        // clear the array
         for(let i = 0; i < highscoresArr.length; i++){
             highscoresArr.pop();
         }
+        // remove the highscore <li>s from the highscores card.
         highScoresOl.innerHTML = '';
 
     })
-    // if the user wants to see high scores, then hide the other cards and display the score card
+    // intialize event listener to view high scores
     document.getElementById('view-highscores').addEventListener('click', function(e){
         e.preventDefault();
-        let scoresRendered = false;
-        if(!scoresRendered){
-            highScoresOl.innerHTML = '';
-            let i = 0;
-            for(let scoreFromArr of highscoresArr){
-                i++;
-                let newEntry = document.createElement('li');
-                newEntry.setAttribute('class','highscore-li')
-                newEntry.textContent = `${i}) ${scoreFromArr.initials} got ${scoreFromArr.scoreIn} points`
-                highScoresOl.appendChild(newEntry);
-                highScoresOl.appendChild(document.createElement('br'))
-            }
-            scoresRendered = true;
+        // clear whatever was rendered before to avoid rendering double
+        highScoresOl.innerHTML = '';
+        // below is a for-let-of loop, but we'll need an increment as well
+        let i = 0;
+        // loop through the scores in the array
+        for(let scoreFromArr of highscoresArr){
+            // increment so we can display the order of the scores
+            i++;
+            // create a new element to append score objets to
+            let newEntry = document.createElement('li');
+            // give this element the highscore-li class for styling purposes
+            newEntry.setAttribute('class','highscore-li')
+            // dismantle the score object and parse them into a string using template literals
+            newEntry.textContent = `${i}) ${scoreFromArr.initials} got ${scoreFromArr.scoreIn} points`
+            // append this new list item to the highscores array
+            highScoresOl.appendChild(newEntry);
         }
+        // hide the review mode cards
         toggleCards(true);
+        // show the highscores card
         document.getElementById('highscores-card').style.display = 'block';
     })
     // undo the previous 
     document.getElementById('go-back').addEventListener('click', function(e){
         e.preventDefault();
+        // show all review mode cards
         toggleCards(false);
+        // hide the highscores card
         document.getElementById('highscores-card').style.display = 'none';
     })
 }
